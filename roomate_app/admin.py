f109f39c79bcf99da66c3c51bd98c15e6c540f6a
Long Tong <longtong@longs-popos.attlocal.net>
2020-08-18 16:48:13 -0700

    Created an admin account

--- roomate_app/admin.py
+++ roomate_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Apartment, Chore
+
+admin.site.register(User)
+admin.site.register(Apartment)
+admin.site.register(Chore)

953d0faf402f3b4249595767e6a4bb94026d346a
Charlie <hcharlie1201@berkeley.edu>
2020-08-18 14:59:47 -0700

    [chore] added into installed_apps

--- room/settings.py
+++ room/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'

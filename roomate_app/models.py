e032804124d05d8a3bb557f565d784a542434ae9
Long Tong <longtong@longs-popos.attlocal.net>
2020-08-18 16:40:42 -0700

    Added Chore model (Tested manually...)

--- roomate_app/models.py
+++ roomate_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import json
 
 # Create your models here.
 
@@ -21,6 +22,24 @@ class User(models.Model):
 
     class Meta:
         verbose_name_plural = "users"
+
+    def __str__(self):
+        return self.name
+
+class Chore(models.Model):
+    name = models.CharField(max_length=100)
+    creator = models.CharField(max_length=100)
+    assignees = models.CharField(max_length=500) #optional
+    complete = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def set_assignees(self, nameList):
+        self.assignees = json.dumps(nameList)
+    def get_assignees(self):
+        return json.loads(self.assignees)
+
+    class Meta: 
+        verbose_name_plural = "chores"
 
     def __str__(self):
         return self.name

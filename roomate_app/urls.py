a7fbaa8b2dd79e48d11778d099eb936ad07009d5
Long Tong <longtong@longs-popos.attlocal.net>
2020-08-18 19:50:02 -0700

    Allow user to create a new apartment

--- roomate_app/urls.py
+++ roomate_app/urls.py
@@ -6,4 +6,5 @@ app_name = 'roomate_app'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    path('newapt/', views.new_apt, name='newApartment'),
 ]
--- roomate_app/views.py
+++ roomate_app/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Apartment, MyUser, Chore
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     return render(request, 'roomate_app/index.html')
+
+def dashboard(request):
+    #check if the user has an apartment or not
+    pass
+#Create a new apartment.
+def new_apt(request):
+    my_apartment = Apartment()
+    current_user = request.user
+    current_user.myuser.myApt = my_apartment
+    current_user.save()
+    return redirect('roomate_ap:dashboard')
+
+#Get an existing apartment.
+def assign_apt(request):
+    current_user = request.user
+    #do something
+    return redirect('roomate_ap:dashboard')
+

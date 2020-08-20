e1886c1997e9a8f7770b00fd3fd8c9b8157e6e9d
Long Tong <longtong@longs-popos.attlocal.net>
2020-08-19 19:29:20 -0700

    [bug] fixed the success_url bug in urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth import views as views_builtin
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from . import forms
 
 app_name = 'users'
@@ -9,5 +10,6 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     # Registration page.
     path('register/', CreateView.as_view(template_name='registration/register.html',
-        form_class=forms.RegisterUserForm, success_url='dashboard/'), name='register'),
+        form_class=forms.RegisterUserForm, 
+        success_url=reverse_lazy('roomate_app:dashboard')), name='register'),
 ]

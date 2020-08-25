d524f9acac0e3b8050bd267e902c4ff627821a25
Charlie <hcharlie1201@berkeley.edu>
2020-08-24 17:45:41 -0700

    [feat] added profile

--- roomate_app/urls.py
+++ roomate_app/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('newapt/', views.new_apt, name='newApartment'),
     path('joinapt/', views.assign_apt, name='joinApartment'),
-    path('newchore/', views.new_chore, name='newChore'),
+    path('profile/', views.profile, name='profile'),
+    path('newchore/', views.new_chore, name='newChore')
 ]

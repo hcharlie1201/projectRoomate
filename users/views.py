5bbeb40953231fb3d016eb4e0a9198d502f5d4ab
Long Tong <longtong@longs-popos.attlocal.net>
2020-08-24 18:13:36 -0700

    [feature] backend for changing password

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, update_session_auth_hash
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from roomate_app.models import MyUser 
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from . import forms
 # Create your views here.
 
@@ -18,3 +19,19 @@ def register(request):
     form = forms.RegisterUserForm()
     messages.warning(request, 'Failed To Create A Profile. Please Try Again.')
     return render(request, 'register.html', {'form':form})
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+
+        form = forms.PasswordChangeForm(user=request.user, data=request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Failed To Create A Profile. Please Try Again.')
+            return redirect('roomate_app:dashboard')
+
+    form = forms.PasswordChangeForm(user=request.user)
+    return render(request, 'registration/change_password.html', {'form':form})
+
